b9767b8f6da1cb23bedcc992090b35c468abbf7a
Samuel <samuelpl0888@gmail.com>
2023-02-17 16:56:44 -0400

    Add sketch of updateCurrentUser controller

--- controllers/account.js
+++ controllers/account.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+const { BadRequestError } = require('../errors');
+
 // Get current loged in user info
 const getCurrentUser = async (req, res) => {
   const user = req.user; // * Info stored in request object (Auth middleware)
@@ -7,4 +10,15 @@ const getCurrentUser = async (req, res) => {
     throw new UnauthenticatedError('User not authenticated');
   }
   res.status(200).send({ status: 'success', user });
+};
+
+const updateCurrentUser = async (req, res) => {
+  //   const {
+  //     body: { email },
+  //     user: { userId },
+  //   } = req;
+  //   if (!email) {
+  //     throw new BadRequestError('Please provide email');
+  //   }
+  //   const user = User.findOneAnd;
 };
